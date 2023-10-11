950c750bed9a6ad53901e4ddedad38c9a9dcf2d5
Mike Jones <mike@netsplit.org.uk>
2023-10-11 18:53:41 +0100

    Add prerequisites script

---
+++ Makefile
@@ -0,0 +1,2 @@
+install_prerequisites:
+	./scripts/install-prerequisites.sh
---
+++ scripts/install-prerequisites.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -euo pipefail;
+
+command_exists () {
+  command -v $1 &> /dev/null;
+}
+
+is_fedora () {
+  command_exists 'dnf';
+}
+
+is_arch () {
+  command_exists 'pacman';
+}
+
+is_debian () {
+  command_exists 'apt';
+}
+
+install_packages () {
+  sudo $1 install ${PACKAGES[*]};
+}
+
+install_antigen () {
+  curl -L git.io/antigen > $HOME/.config/zsh/antigen.zsh;
+}
+
+# Dependencies for programs configured by this repository
+PACKAGES=(
+  'emacs'
+  'git'
+  'tmux'
+  'zsh'
+);
+
+COMMAND='';
+
+if is_fedora;
+then
+  COMMAND='dnf';
+elif is_arch;
+then
+  COMMAND='pacman';
+elif is_debian;
+then
+  COMMAND='apt';
+fi
+
+mkdir -p $HOME/.config/zsh;
+
+install_packages $COMMAND;
+install_antigen;
